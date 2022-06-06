85bbc5de9e961a7ba3bbac6e60343e9aa3515190
Tobias Hallingstad <Tobias@hallingstad.me>
2022-06-06 04:54:39 +0200

    Removed Node default

--- src/utils/util.rs
+++ src/utils/util.rs
@@ -4,14 +4,3 @@ pub struct Node<T> {
     pub(crate) left: Option<* mut Node<T>>,
     pub(crate) count: i8,
 }
-
-impl<T> Default for Node<T> {
-    fn default() -> Self {
-        Node {
-            data: 1,
-            right: None,
-            left: None,
-            count: 0
-        }
-    }
-}
