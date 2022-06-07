abaedc191bb93681ec245c7838be771464554c98
Tobias Hallingstad <Tobias@hallingstad.me>
2022-06-07 15:15:16 +0200

    Moved to a more generic place

--- src/unbalanced.rs
+++ src/unbalanced.rs
@@ -1,2 +1,22 @@
 pub mod tree;
 pub mod array;
+
+pub(crate) trait Insert<T> {
+    fn insert(&mut self, data: T);
+    fn insert_vec(&mut self, data: Vec<T>);
+}
+
+pub(crate) trait Counting {
+    fn num_nodes(&self) -> u32;
+    fn num_leaves(&self) -> u32;
+    fn num_two_children(&self) -> u32;
+    fn num_levels(&self) -> u32;
+}
+
+pub(crate) trait Remove<T> {
+    fn remove(&mut self, data: T);
+}
+
+pub(crate) trait Contains<T> {
+    fn contains(&self, data: T) -> bool;
+}
--- src/unbalanced/array.rs
+++ src/unbalanced/array.rs
@@ -2,26 +2,6 @@ use std::collections::VecDeque;
 
 pub mod binary_tree;
 
-pub(crate) trait Insert<T> {
-    fn insert(&mut self, idx: usize, data: T);
-    fn insert_vec(&mut self, idx: usize, data: Vec<T>);
-}
-
-pub(crate) trait Remove<T> {
-    fn remove(&mut self, data: T);
-}
-
-pub(crate) trait Contains<T> {
-    fn contains(&self, data: T) -> bool;
-}
-
-pub(crate) trait Counting {
-    fn num_nodes(&self) -> i8;
-    fn num_leaves(&self) -> i8;
-    fn num_two_children(&self) -> i8;
-    fn num_levels(&self) -> i8;
-}
-
 pub(crate) trait Traversal<T> {
     fn pre_order(&self) -> Vec<T>;
     fn in_order(&self) -> Vec<T>;
@@ -29,16 +9,14 @@ pub(crate) trait Traversal<T> {
     fn bfs(&self) -> Vec<T>;
 }
 
-pub(crate) trait Print<T> {
-    fn print(&self, f: fn () -> Vec<T>);
-    fn println(&self, f: fn () -> Vec<T>);
-}
-
 pub(crate) trait Util<T> {
     fn clear(&mut self, idx: usize);
 
-    fn index_out(&self, idx: usize, op: fn (bool, bool) -> bool) -> bool;
-    fn next_index_out(&self, idx: usize, op: fn (bool, bool) -> bool) -> bool;
+    fn index_out(&self, idx: usize) -> bool;
+    fn next_index_out(&self, idx: usize) -> bool;
+
+    fn index_out_f(&self, idx: usize, op: fn (bool, bool) -> bool) -> bool;
+    fn next_index_out_f(&self, idx: usize, op: fn (bool, bool) -> bool) -> bool;
 
     fn left_child(&self, idx: usize) -> usize;
     fn right_child(&self, idx: usize) -> usize;
