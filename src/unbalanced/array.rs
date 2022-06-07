8d9ea8a0dcca7db087915abd5ddacccf51047755
Tobias Hallingstad <Tobias@hallingstad.me>
2022-06-08 01:38:48 +0200

    Removed Traversal trait and added traits to manipulate at index

--- src/unbalanced/array.rs
+++ src/unbalanced/array.rs
@@ -2,15 +2,8 @@ use std::collections::VecDeque;
 
 pub mod binary_tree;
 
-pub(crate) trait Traversal<T> {
-    fn pre_order(&self) -> Vec<T>;
-    fn in_order(&self) -> Vec<T>;
-    fn post_order(&self) -> Vec<T>;
-    fn bfs(&self) -> Vec<T>;
-}
-
 pub(crate) trait Util<T> {
-    fn clear(&mut self, idx: usize);
+    fn clear_from(&mut self, idx: usize);
 
     fn index_out(&self, idx: usize) -> bool;
     fn next_index_out(&self, idx: usize) -> bool;
@@ -25,4 +18,21 @@ pub(crate) trait Util<T> {
     fn add_children_to_queue(&self, idx: usize, q: &mut VecDeque<usize>);
 
     fn increase_levels(&mut self, amount: u32);
+}
+
+pub(crate) trait InsertAt<T>
+{
+    fn insert_at(&mut self, idx: usize, data: T);
+    fn insert_vec_at(&mut self, idx: usize, data: Vec<T>);
+}
+
+pub(crate) trait Clear<T> {
+    fn clear_at(&mut self, idx: usize);
+}
+
+pub(crate) trait TraversalFrom<T> {
+    fn pre_order_from(&self, idx: usize) -> Vec<T>;
+    fn in_order_from(&self, idx: usize) -> Vec<T>;
+    fn post_order_from(&self, idx: usize) -> Vec<T>;
+    fn bfs_from(&self, idx: usize) -> Vec<T>;
 }
