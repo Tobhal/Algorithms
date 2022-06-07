e0c919366ffa0e543bff7626cf3405884e2b8106
Tobias Hallingstad <Tobias@hallingstad.me>
2022-06-08 01:37:43 +0200

    Printing more info

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@
 #![allow(non_snake_case)]
 
 use crate::unbalanced::array::binary_tree::BinaryTree;
-use crate::unbalanced::{Counting, Insert};
+use crate::unbalanced::{Counting, Insert, Remove, Traversal};
+use crate::unbalanced::array::TraversalFrom;
 use crate::utils::util::Node;
 
 mod unbalanced;
 mod utils;
+mod tests;
 
 fn main() {
     let mut root = BinaryTree::new();
@@ -29,4 +31,21 @@ fn main() {
     println!("Leaves:       {}", root.num_leaves());
     println!("Two children: {}", root.num_two_children());
     println!("Levels:       {}", root.num_levels());
+
+    println!("\n");
+    println!("Order:");
+    print!("Pre:  ");
+    for e in root.pre_order_from(2) { print!("{} ", e); }
+
+    println!();
+    print!("In:   ");
+    for e in root.in_order() { print!("{} ", e); }
+
+    println!();
+    print!("Post: ");
+    for e in root.post_order() { print!("{} ", e); }
+
+    println!();
+    print!("BFS:  ");
+    for e in root.bfs() { print!("{} ", e); }
 }
